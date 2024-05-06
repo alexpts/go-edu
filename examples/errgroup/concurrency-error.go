37e792b92bbb9f11cd033da06d95398d9342c7b0
alexpts <alexpts@yandex.ru>
2024-05-06 08:34:21 +0300

    add example for error group concurrency-error.go

---
+++ examples/errgroup/concurrency-error.go
@@ -0,0 +1,51 @@
+package main
+
+import (
+	"context"
+	"errors"
+	"fmt"
+	"math/rand"
+	"sync"
+	"time"
+
+	"golang.org/x/sync/errgroup"
+)
+
+func main() {
+	g, gCtx := errgroup.WithContext(context.Background())
+	g.SetLimit(3)
+
+	m := sync.Mutex{}
+	var joinErrors error
+
+	for i := 0; i < 20; i++ {
+		localI := i
+		g.Go(func() error {
+			if gCtx.Err() != nil {
+				fmt.Printf("%d пропускаем горутину\n", localI)
+				return nil
+			}
+
+			fmt.Printf("%d выполняем горутину\n", localI)
+			r := rand.Intn(3000)
+			time.Sleep(time.Duration(r) * time.Millisecond)
+
+			m.Lock()
+			defer m.Unlock()
+
+			if i > 3 {
+				joinErrors = errors.Join(joinErrors, errors.New("some error"))
+			}
+
+			return joinErrors
+		})
+	}
+
+	if err := g.Wait(); err != nil {
+		fmt.Println("111=====")
+		fmt.Printf("\n\n%v\n\n", err)
+	}
+
+	fmt.Println("222=====")
+	fmt.Printf("%v\n", joinErrors)
+}
---
+++ examples/go.mod
@@ -0,0 +1,5 @@
+module github.com/alexpts/edu-go/examples
+
+go 1.22
+
+require golang.org/x/sync v0.7.0
